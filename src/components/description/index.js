a00a141ae019eb820cd943c14e093df8ce4a8937
sulivann <nguyen.sulivan@gmail.com>
2020-06-20 09:49:09 +0700

    changes

--- src/components/description/index.js
+++ src/components/description/index.js
@@ -5,7 +5,9 @@ export default class Description extends Component {
   render() {
     return (
         <div class={style.description}>
-          Hello, I'm a front-end developer based in Paris. Currently in my fourth year in HETIC. I am looking for an internship from July to December.
+          Hello, I'm a front-end developer based in Paris.
+          <br />
+          Currently working as a software engineer at Osmo.
         </div>
     );
   }
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -19,16 +19,16 @@ export default class Home extends Component {
             <div class={style.mobile}>
               <Label text='selected work'/>
               <div class={style.works}>
-                <a href="http://dev.sulivannguyen.fr/dda/" target="_blank">
+                <div>
                   <Work
                     label='David Taing 2017'
                     img='../../assets/img/td.jpg'/>
-                </a>
-                <a href="http://preprod.sulivannguyen.fr/" target="_blank">
+                </div>
+                <div>
                   <Work
                     label='Gauthier Drllx 2018'
                     img='../../assets/img/gd.jpg'/>
-                </a>
+                </div>
               </div>
             </div>
             <div class={style.about}>
